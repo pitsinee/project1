1a407860769f9e8b2f6cc49d22fbac0e707c49c4
PurinPhangkaew <purinphangkaew@gmail.com>
2021-10-07 15:22:28 +0700

    test1 def

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 def sum(int) :
     x = 0
-    for i in range(int) :
+    for i in range(1, int+1) :
        x += i
-    print(x) 
+    return(x) 
 
-sum(10)
+#sum(10)
+
+def test1(int) :
+    x = ''
+    for i in range(1, int+1) :
+        if i == 10 :
+            y = str(i)
+            x += y
+        else :
+            y = str(i) + '+'
+            x += y
+    print(x, '=', sum(int))
+
+test1(10)
