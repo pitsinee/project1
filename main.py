3843b845e25fc8195f28013dd5691945e283dfd4
PurinPhangkaew <purinphangkaew@gmail.com>
2021-10-07 16:05:50 +0700

    fix bug

--- main.py
+++ main.py
@@ -6,15 +6,18 @@ def sum(int) :
 
 #sum(10)
 
-def test1(int) :
-    x = ''
-    for i in range(1, int+1) :
-        if i == 10 :
-            y = str(i)
-            x += y
-        else :
-            y = str(i) + '+'
-            x += y
-    print(x, '=', sum(int))
+def test1(a) :
+    if type(a) is int :
+        x = ''
+        for i in range(1, a+1) :
+            if i == 10 :
+                y = str(i)
+                x += y
+            else :
+                y = str(i) + '+'
+                x += y
+        print(x, '=', sum(a))
+    else :
+        print('input is not int')
 
-test1(10)
+test1('aa')
